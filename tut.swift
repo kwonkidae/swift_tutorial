ff9c02eb4d994e1b0b68583c63c16472b8ec42fc
kwon <kkdosk@naver.com>
2018-03-17 23:21:44 +0900

    add

--- tut.swift
+++ tut.swift
@@ -36,5 +36,56 @@ var occupations = [
 let emptyArray = [String]()
 let emptyDictionary = [String: Float]()
 
-print(emptyArray)
-print(emptyDictionary)
+// print(emptyArray)
+// print(emptyDictionary)
+
+let indivisualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in indivisualScores {
+	if score > 50 {
+		teamScore += 3
+	} else {
+		teamScore += 1
+	}
+}
+
+print(teamScore)
+
+var optionalString: String? = nil
+print(optionalString == nil)
+
+var optionalName: String?
+var greeting = "Hello!"
+if let name = optionalName {
+	greeting = "Hello, \(name)"
+	print(greeting)
+}
+// print(optionalName as Any)
+
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+	print("Add some riisins")
+case "cucumber", "watercress":
+	print("That would make a good tea sanwitch.")
+case let x where x.hasSuffix("pepper"):
+	print("Is it a spucy\(x)?")
+default:
+	print("Everything tastes good in soup.")
+}
+
+let interestingNumbers = [
+	"Prime": [2,3, 5, 7, 11, 13],
+	"Fibonacci": [1, 1, 2, 3, 5, 8],
+	"Square": [1, 4, 9, 16, 25],
+]
+
+var largest = 0
+for (_, numbers) in interestingNumbers {
+	for number in numbers {
+		if number > largest {
+			largest = number
+		}
+	}
+}
+print(largest)
