cdf80630695231625b09cea8839d9e32fe4d24cf
kwon <kkdosk@naver.com>
2018-03-18 00:33:00 +0900

    commit

--- fun.swift
+++ fun.swift
@@ -31,3 +31,39 @@ func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
 let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
 print(statistics.0)
 print(statistics.1)
+
+func returnFifteen() -> Int {
+	var y = 10
+	func add() {
+		y += 5
+	}
+	add()
+	return y
+}
+
+print(returnFifteen())
+
+func makeIncrementer() -> ((Int) -> Int) {
+	func addOne(number: Int) -> Int {
+		return 1 + number
+	}
+	return addOne
+}
+
+var increment = makeIncrementer()
+print(increment(7))
+
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+	for item in list {
+		if condition(item) {
+			return true
+		}
+	}
+	return false
+}
+
+func lessThanTen(number: Int) -> Bool {
+	return number < 10
+}
+var numbers = [20, 19, 11, 12]
+print(hasAnyMatches(list: numbers, condition: lessThanTen))
