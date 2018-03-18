412849ded91d222632dd99f8430e3d5e2ceb96e4
kwon <kkdosk@naver.com>
2018-03-18 23:44:02 +0900

    c

--- enum.swift
+++ enum.swift
@@ -28,3 +28,38 @@ if let convertedRank = Rank(rawValue: 3) {
 	let threeDescription = convertedRank.simpleDescription()
 	print(threeDescription)
 }
+
+enum Suit {
+	case spades, hearts, diamonds, clubs
+	func simpleDescription() -> String {
+		switch self {
+		case .spades:
+			return "spades"
+		case .hearts:
+			return "hearts"
+		case .diamonds:
+			return "diamonds"
+		case .clubs:
+			return "clubs"
+		}
+	}
+}
+
+let hearts = Suit.hearts
+let heartsDescription = hearts.simpleDescription()
+print(heartsDescription)
+
+enum ServerResponse {
+	case result(String, String)
+	case failure(String)
+}
+
+let success = ServerResponse.result("6:00 am", "8:09 pm")
+let failure = ServerResponse.failure("Out of cheese.")
+
+switch success {
+case let .result(sunrise, sunset):
+	print("Sunrise is at \(sunrise) and sunset is at \(sunset).")
+case let .failure(message):
+	print("Failure... \(message)")
+}
