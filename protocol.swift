85dcd69869068a1ffdd750e75fa82fdd7900e123
kkdosk <kkdosk@naver.com>
2018-03-19 23:40:12 +0900

    c

--- protocol.swift
+++ protocol.swift
@@ -3,3 +3,28 @@ protocol ExampleProtocol {
 	var simpleDescription: String { get }
 	mutating func adjust()
 }
+
+class SimpleClass: ExampleProtocol {
+	var simpleDescription: String = "A very simple class."
+	var anotherProperty: Int = 69105
+	func adjust() {
+		simpleDescription += " Now 100% adjusted."
+	}
+}
+
+var a = SimpleClass()
+a.adjust()
+let aDescription = a.simpleDescription
+print(aDescription)
+
+struct SimpleStructure: ExampleProtocol {
+	var simpleDescription: String = "A simple structure"
+	mutating func adjust() {
+		simpleDescription += " (adjusted)"
+	}
+}
+
+var b = SimpleStructure()
+a.adjust()
+let bDescription = b.simpleDescription
+print(bDescription)
