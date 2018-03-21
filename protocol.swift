3cfcd2b5e906d5845fa24b752cc340376a78b8bd
kkdosk <kkdosk@naver.com>
2018-03-21 22:46:57 +0900

    adf

--- protocol.swift
+++ protocol.swift
@@ -28,3 +28,14 @@ var b = SimpleStructure()
 a.adjust()
 let bDescription = b.simpleDescription
 print(bDescription)
+
+extension Int: ExampleProtocol {
+	var simpleDescription: String {
+		return "The number \(self)"
+	}
+	mutating func adjust() {
+		self += 42
+	}
+}
+
+// print(7.simpleDescription)
