abfecabd5d351470a056390c4dfc48bd46d8ef0e
kkdosk <kkdosk@naver.com>
2018-03-31 23:13:22 +0900

    m

--- closure.swift
+++ closure.swift
@@ -21,3 +21,40 @@ reversedNames = names.sorted(by: { s1, s2 in s1 > s2})
 
 reversedNames = names.sorted(by: {$0 > $1})
 print(reversedNames)
+
+func someFunctionThatTakesAClosure(closure: () -> Void) {
+	closure()
+}
+
+someFunctionThatTakesAClosure(closure: {
+	print("some closure")
+})
+
+someFunctionThatTakesAClosure() {
+	print("some closure2")
+}
+
+reversedNames = names.sorted() {$0 > $1}
+print(reversedNames)
+
+reversedNames = names.sorted { $0 > $1 }
+print(reversedNames)
+
+let digitNames = [
+	0: "Zero", 1: "One", 2: "Two",   3: "Three", 4: "Four",
+	5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
+]
+
+let numbers = [16, 58, 510]
+
+let strings = numbers.map { (number) -> String in
+	var number = number
+	var output = ""
+	repeat {
+		output = digitNames[number % 10]! + output
+		number /= 10
+	} while number > 0
+	return output
+}
+
+print(strings)
